5543157de8b9179c8706d1f645c9c342d1ff3119
Florence-Bukola <florencebukola046@gmail.com>
2025-11-13 11:16:24 +0100

    Delete src folder from deployment branch

--- src/api/fetchSubscription.ts
+++
@@ -1,9 +0,0 @@
-import { mockSubscriptions, type Subscription } from '../api/mock-data'
-
-
-export async function fetchSubscriptions(): Promise<Subscription[]> {
-// simulate network delay
-await new Promise((res) => setTimeout(res, 1000))
-// for error simulation 
-return mockSubscriptions
-}
